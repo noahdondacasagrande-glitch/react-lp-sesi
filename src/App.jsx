c4b32e58687fa092c7d009d1008f82e3dc971bbb
noahdondacasagrande-glitch <noahdondacasagrande@gmail.com>
2026-05-22 11:06:24 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,81 @@
-import { useEffect } from 'react'
-import s from './App.module.css'
+import { useEffect } from "react";
+import s from "./App.module.css";
 
 function App() {
-
   useEffect(() => {
     const handleScroll = () => {
-      const nav = document.querySelector('nav')
-      const footer = document.querySelector('footer')
+      const nav = document.querySelector("nav");
+      const footer = document.querySelector("footer");
 
       if (window.scrollY > 0) {
-        nav.classList.add('cs')
-        footer.classList.add('ft')
+        nav.classList.add("cs");
+        footer.classList.add("ft");
       } else {
-        nav.classList.remove('cs')
-        footer.classList.remove('ft')
+        nav.classList.remove("cs");
+        footer.classList.remove("ft");
       }
-    }
+    };
 
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   function navTo(id) {
     document.getElementById(id).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
+      behavior: "smooth",
+      block: "start",
+    });
   }
 
   function enviar() {
-    alert('Mensagem enviada!')
+    alert("Mensagem enviada!");
   }
 
   return (
     <>
-      <nav>
-        <span className={s.navitems} onClick={() => navTo("s1")}>
-          <img src="/casas.png" alt="JOGO" width="30px" />
-          <a href="#s1" className={s.navwords}>JOGO</a>
-        </span>
-
-        <span className={s.navitems} onClick={() => navTo("s2")}>
-          <img src="/caixa.png" alt="Carros" width="30px" />
-          <a href="#s2" className={s.navwords}>Carros</a>
-        </span>
-
-        <span className={s.navitems} onClick={() => navTo("s3")}>
-          <img src="/video.png" alt="Video" width="30px" />
-          <a href="#s3" className={s.navwords}>Video</a>
-        </span>
-
-        <span className={s.navitems} onClick={() => navTo("s4")}>
-          <img src="/contato.png" alt="Contato" width="30px" />
-          <a href="#s4" className={s.navwords}>Contato</a>
-        </span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            NDF
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="s1" onClick={() => navTo("#s1")}>
+                Home
+              </a>
+              <a className="nav-link" href="#s2" onClick={() => navTo("s2")} >
+                Features
+              </a>
+              <a className="nav-link" href="#s3"onClick={() => navTo("s3")}>
+                Pricing
+              </a>
+              <a className="nav-link " href="#s4"onClick={() => navTo("s4")}>
+                Disabled
+              </a>
+            </div>
+          </div>
+        </div>
       </nav>
 
       <main>
-
         <section className={s.s1} id="s1">
           <h1>need for speed</h1>
           <h2>best racing game of all time</h2>
         </section>
 
         <section className={s.s2} id="s2">
-
           <div className={s.card}>
             <img src="/01.jpg" alt="carro 1" width="300px" />
             <div>
@@ -89,7 +99,6 @@ function App() {
               <p>Lorem ipsum dolor sit amet consectetur.</p>
             </div>
           </div>
-
         </section>
 
         <section className={s.s3} id="s3">
@@ -105,11 +114,9 @@ function App() {
         </section>
 
         <section className={s.s4} id="s4">
-
           <h3>Contato</h3>
 
           <div className={s.formulario}>
-
             <label htmlFor="nome">Nome:</label>
             <input type="text" id="nome" placeholder="Digite seu nome" />
 
@@ -117,11 +124,8 @@ function App() {
             <textarea id="assunto" placeholder="Digite uma mensagem"></textarea>
 
             <button onClick={enviar}>ENVIAR</button>
-
           </div>
-
         </section>
-
       </main>
 
       <footer>
@@ -138,7 +142,7 @@ function App() {
         </a>
       </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
